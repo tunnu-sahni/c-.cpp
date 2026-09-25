b748d566c2afef65f84564e86b1b3c59a8b04311
tunnu-sahni <tsahni313@gmail.com>
2026-09-25 22:24:43 +0530

    cmnt.cpp

---
+++ OneDrive/Desktop/c++.cpp/cmnt.cpp
@@ -0,0 +1,71 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     // this is a comment
+//     cout << "Hello world";
+//     return 0;
+// }
+
+//variable
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int myNum = 15;
+//     cout << myNum;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int myNum = 15;
+//     cout << myNum;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int myNum = 15;
+//     myNum = 10;
+//     cout << myNum;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int myAge = 21;
+//     cout << "I am " << myAge << " years old.";
+//     return 0;
+// }
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main() {
+//     string name = "tunnu";
+//     int age = 21;
+//     double height = 5.4;
+
+//     cout << name << " is " << age << " years old and " << height << " feet tall.";
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    cout << sum;
+    return 0;
+}
