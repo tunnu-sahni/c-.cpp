2743ef7630aa273eb3aa1cf6bef3ba28193f62a4
tunnu-sahni <tsahni313@gmail.com>
2026-09-26 22:08:09 +0530

    data_type.cpp

---
+++ OneDrive/Desktop/c++.cpp/data_type.cpp
@@ -0,0 +1,102 @@
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main() {
+//     int myNum = 5;
+//     float myFloatNum = 5.59;
+//     double myDoubleNum = 9.98;
+//     char myLetter = 'D';
+//     bool myBoolean = true;
+//     string myString = "Hello";
+
+//     cout << "int:" << myNum << "\n";
+//     cout << "float:" << myFloatNum << "\n";
+//     cout << "char:" << myLetter << "\n";
+//     cout << "string:" << myString << "\n";
+
+//     return 0;
+// }
+
+// numeric types
+// int
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int myNum = 1000;
+//     cout << myNum;
+//     return 0;
+// }
+//float
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     float myNum = 5.75;
+//     cout << myNum;
+//     return 0;
+// }
+//double
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     double myNum = 19.99;
+//     cout << myNum;
+//     return 0;
+// }
+// scientific numbers
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     float f1 = 35e3;
+//     double d1 = 12E4;
+//     cout << f1 << "\n";
+//     cout << d1;
+//     return 0;
+// }
+
+//boolean type
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     bool isCodingFun = true;
+//     bool isFinishTasty = false;
+//     cout << isCodingFun << "\n";
+//     cout << isFinishTasty;
+//     return 0;
+// }
+
+//characters types
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     char myGrade = 'B';
+//     cout << myGrade;
+//     return 0;
+// }
+
+//string types
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// int main() {
+//     string greeting = "Hello";
+//     cout << greeting;
+//     return 0;
+// }
+
+//auto keyword
+#include <iostream>
+using namespace std;
+
+int main() {
+    auto x = 5;
+    cout << x;
+    return 0;
+}
