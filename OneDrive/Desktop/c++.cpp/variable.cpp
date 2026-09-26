6ba5f325612f848972ef98a8b81687abf22b4719
tunnu-sahni <tsahni313@gmail.com>
2026-09-26 17:52:13 +0530

    variable.cpp

--- OneDrive/Desktop/c++.cpp/variable.cpp
+++ OneDrive/Desktop/c++.cpp/variable.cpp
@@ -26,13 +26,38 @@
 //     return 0;
 // }
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
-    int x = 5;
-    int y = 7;
-    int sum = x + y;
-    cout << sum;
-    return 0;
-}
+// int main() {
+//     int x = 5;
+//     int y = 7;
+//     int sum = x + y;
+//     cout << sum;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int x, y, z;
+//     x = y = z = 50;
+//     cout << x + y + z;
+//     return 0;
+// }
+//identifiers
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     //good variable name
+//     int minutesPerHour = 60;
+
+//     //ok, but not so easy to understand 
+//     int m = 60;
+
+//     cout << minutesPerHour << "\n";
+//     cout << m << "\n";
+//     return 0;
+// }
+
